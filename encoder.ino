b4104b187c44da55e4397db28ba963d79b563c6e
Peter <tanulo@trefortszki.hu>
2021-10-14 11:35:57 +0200

    encoder mukodes

--- encoder.ino
+++ encoder.ino
@@ -1,9 +1,18 @@
-void setup() {
-  // put your setup code here, to run once:
 
+
+ int sw=2;
+  int outputclk=4;
+  int outputdt=3;
+void setup() {
+ Serial.begin (9600);
+Serial.println("encoder kezelő program");
+for(int i=2;i<=4;i++) pinMode(i, INPUT_PULLUP);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  for(int i=2;i<=4;i++){
+  Serial.print(digitalRead(i));
+  Serial.print("; ");
+  }
+  Serial.println("");
 }
